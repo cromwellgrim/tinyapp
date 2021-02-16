d4a4e321f02bb627c41e9d2acd3a07467c3a3b4c
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-16 00:19:27 +0000

    updated all for longURL sub and generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const PORT = 8080; 
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = function() {
+  /* create random character (Math.random) send it to a string (toString) don't want 0s (replace)
+  and limit to 6 characters until string.length = 6 (substr) used 2 methods found online and combined!
+  https://dev.to/oyetoket/fastest-way-to-generate-random-strings-in-javascript-2k5a
+  */
+    return Math.random().toString(36).replace('0.', '').substr(2, 6);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +31,15 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/hellos", (req, res) => {
+  const templateVars = { greeting: 'Hello World' };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -31,9 +50,9 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hellos", (req, res) => {
-  const templateVars = { greeting: 'Hello World' };
-  res.render("hello_world", templateVars);
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
