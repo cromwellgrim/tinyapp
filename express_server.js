e348d411514aae109c8a3f5ecdaa0114dd5f4153
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-17 22:00:09 +0000

    feature: added in the register page and get/post combo to get there

--- express_server.js
+++ express_server.js
@@ -13,7 +13,7 @@ const generateRandomString = function() {
   https://dev.to/oyetoket/fastest-way-to-generate-random-strings-in-javascript-2k5a
   */
     return Math.random().toString(36).replace('0.', '').substr(2, 6);
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -25,9 +25,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/", (req, res) => {
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("urls_index", templateVars);
+});
+
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_register", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -48,6 +59,11 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+})
+
+app.get("/urls/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_register", templateVars);
 })
 
 app.post("/", (req, res) => {
@@ -101,6 +117,11 @@ app.post("/urls/show", (req, res) => {
 app.post("/urls/new", (req, res) => {
   const templateVars = { username: req.cookies["username"] };
   res.send("urls_new", templateVars);
+});
+
+app.post("/urls/register", (req, res) => {
+  const templateVars = { username: req.cookies["username"] };
+  res.send("urls_register", templateVars);
 });
 
 app.listen(PORT, () => {
