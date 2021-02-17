f736d2133ededc49f7596ee59e9bdbe25113fa6c
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-17 23:58:00 +0000

    chores: removed genRandom() from server.js and exporting from a helper function now

--- express_server.js
+++ express_server.js
@@ -3,21 +3,33 @@ const app = express();
 const PORT = 8080; 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
+const genRandom = require('genRandom.js');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-const generateRandomString = function() {
-  /* create random character (Math.random) send it to a string (toString) don't want 0s (replace)
-  and limit to 6 characters until string.length = 6 (substr) used 2 methods found online and combined!
-  https://dev.to/oyetoket/fastest-way-to-generate-random-strings-in-javascript-2k5a
-  */
-    return Math.random().toString(36).replace('0.', '').substr(2, 6);
+const currentUser = {
+  id: "randomID",
+  email: "email",
+  password: "password"
+}
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "example"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "example2"
+  }
 };
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.ca"
 };
 
 
@@ -26,33 +38,32 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, currentUser: req.cookies["currentUser"] };
   res.render("urls_index", templateVars);
 });
 
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, currentUser: req.cookies["currentUser"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { currentUser: req.cookies["currentUser"] };
   res.render("urls_register", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"]};
+  const templateVars = { currentUser: req.cookies["currentUser"]};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/show", (req, res) => {
-  const templateVars = { username: req.cookies["username"]};
+  const templateVars = { currentUser: req.cookies["currentUser"]};
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], currentUser: req.cookies["currentUser"] };
   res.render("urls_show", templateVars);
 });
 
@@ -62,17 +73,12 @@ app.get("/u/:shortURL", (req, res) => {
 })
 
 app.get("/urls/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { currentUser: req.cookies["currentUser"] };
   res.render("urls_register", templateVars);
 })
 
-app.post("/", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
-  res.send("urls_new", templateVars);
-})
-
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
+  const shortURL = genRandom();
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
@@ -85,7 +91,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.post("/urls/:shortURL", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { currentUser: req.cookies["currentUser"] };
   res.send("urls_new", templateVars);
 })
 
@@ -97,31 +103,45 @@ app.post("/urls/:id/edit", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username; 
-  res.cookie('username', username);
+  const currentUser = req.body.currentUser; 
+  res.cookie('currentUser', currentUser);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  const username = req.body.username;
-  res.cookie('username', username)
-  res.clearCookie('username')
+  const currentUser = req.body.currentUser;
+  res.cookie('currentUser', currentUser)
+  res.clearCookie('currentUser')
   res.redirect("/urls");
 })
 
 app.post("/urls/show", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { currentUser: req.cookies["currentUser"] };
   res.send("/urls/show", templateVars)
 });
 
 app.post("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { currentUser: req.cookies["currentUser"] };
   res.send("urls_new", templateVars);
 });
 
+// app.post("/urls/register", (req, res) => {
+//   const templateVars = { currentUser: req.cookies["currentUser"] };
+//   res.send("urls_register", templateVars);
+// });
+
 app.post("/urls/register", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
-  res.send("urls_register", templateVars);
+  const currentUser = { 
+    randomID: genRandom(),
+    email: req.body.email,
+    password: req.body.password
+  };
+  res.redirect("/urls/", currentUser);
+});
+
+app.post("*", (req, res) => {
+  res.status(404).send(body);
+  res.render("FOUR ZERO FOUR")
 });
 
 app.listen(PORT, () => {
---
+++ genRandom.js
@@ -0,0 +1,11 @@
+const genRandom = function() {
+  /* create random character (Math.random) send it to a string (toString) don't want 0s (replace)
+  and limit to 6 characters until string.length = 6 (substr) used 2 methods found online and combined!
+  https://dev.to/oyetoket/fastest-way-to-generate-random-strings-in-javascript-2k5a
+  */
+    return Math.random().toString(36).replace('0.', '').substr(2, 6);
+};
+
+
+
+module.exports = genRandom();
