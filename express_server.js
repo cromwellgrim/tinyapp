932aa539b167b87a9fd7911a21af6191767d47a6
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-18 06:32:04 +0000

    chores: fixed login page range error

--- express_server.js
+++ express_server.js
@@ -55,6 +55,11 @@ app.get("/urls/register", (req, res) => {
   res.render("urls_register", templateVars);
 });
 
+app.get("/urls/login", (req, res) => {
+  const templateVars = { user: req.cookies["user"] };
+  res.render("urls_login", templateVars);
+});
+
 app.get("/urls/new", (req, res) => {
   const templateVars = { user: req.cookies["user"]};
   res.render("urls_new", templateVars);
@@ -73,12 +78,12 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
 
 app.get("/urls/register", (req, res) => {
   const templateVars = { user: req.cookies["user"] };
   res.render("urls_register", templateVars);
-})
+});
 
 // this is for generating shortURLs
 app.post("/urls", (req, res) => {
@@ -106,18 +111,12 @@ app.post("/urls/:id/edit", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/login", (req, res) => {
-  const user = req.body.user; 
-  res.cookie('user', user);
-  res.redirect("/urls");
-});
-
 app.post("/logout", (req, res) => {
   const user = req.body.user;
   res.cookie('user', user);
   res.clearCookie('user');
   res.redirect("/urls");
-})
+});
 
 app.post("/urls/show", (req, res) => {
   const templateVars = { user: req.cookies["user"] };
@@ -129,7 +128,12 @@ app.post("/urls/new", (req, res) => {
   res.send("urls_new", templateVars);
 });
 
-app.post("/urls/register", (req, res, next) => {
+app.post("/urls/login", (req, res) => {
+  const templateVars = { user: req.cookies["user"] };
+  res.send("urls_login", templateVars);
+});
+
+app.post("/urls/register", (req, res) => {
   const id = genRandom()
   users[id] = { id: id,
     email: req.body.email,
@@ -150,8 +154,8 @@ app.post("/urls/register", (req, res, next) => {
 });
 
 app.post("*", (req, res) => {
-  res.status(404).send(body);
-  res.render("FOUR ZERO FOUR")
+  res.status(404)
+  res.send("FOUR ZERO FOUR")
 });
 
 app.listen(PORT, () => {
